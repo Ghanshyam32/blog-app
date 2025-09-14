d7859afd6afd305c669a3799792f220753f87906
Ghanshyam32 <ghanshyammishra3205615@gmail.com>
2025-09-14 21:02:44 +0530

    CORS

--- src/main/java/com/ghanshyam/blogera/config/WebConfig.java
+++ src/main/java/com/ghanshyam/blogera/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173") // Allow frontend origin
+                .allowedOrigins("https://studio--studio-1346424115-35dc3.us-central1.hosted.app") // Allow frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies, if needed
